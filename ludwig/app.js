b977b397dd305b3eab63f9fae2ba150763526aa9
Jamie Hughes <jamie.hughes42@btinternet.com>
2021-03-25 21:46:59 +0000

    Update app.js

--- ludwig/app.js
+++ ludwig/app.js
@@ -1,7 +1,7 @@
 const displayArea = document.querySelector('.grid');
 const subText = document.querySelector('.sub-text');
 
-const validDays = ['D1', 'D2', 'D3', 'D4'];
+const validDays = ['D1', 'D2', 'D3', 'D4', 'D5', 'D6'];
 
 function generateTable(tableID, headers, data) {
     const tableElement = document.createElement('table');
